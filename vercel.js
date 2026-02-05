ffc52fe5c6291b153e334ee3e3d2d331802d5e75
LUCAS-LUIZ-ROCHA <lucasluiz_ads@hotmail.com>
2026-02-05 00:14:15 -0300

    deploy | add - vercel.js | sessão portfólio - concluída | Roda pé - concluido

---
+++ vercel.js
@@ -0,0 +1,29 @@
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "app.py",
+      "use": "@vercel/python"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "app.py"
+    }
+  ],
+  "env": {
+    "PYTHON_VERSION": "3.11"
+  },
+  "headers": [
+    {
+      "source": "/static/(.*)",
+      "headers": [
+        {
+          "key": "Cache-Control",
+          "value": "public, max-age=31536000, immutable"
+        }
+      ]
+    }
+  ]
+}
